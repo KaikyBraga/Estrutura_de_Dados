de5fe3f78d73e4acad272886eb6d3ac5b64dc7bd
Kaiky Braga <126474113+KaikyBraga@users.noreply.github.com>
2024-06-05 08:32:18 -0300

    Update aula-19.cpp

--- aula-19.cpp
+++ aula-19.cpp
@@ -218,3 +218,214 @@ void traversePostOrder(Node* ptrStartingNode)
         cout << " " << ptrStartingNode->iPayload;
     }
 }
+
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+
+#include <iostream>
+
+using namespace std;
+
+typedef struct Node
+{
+    int iPayload;
+    Node* ptrLeft;
+    Node* ptrRight;
+} Node;
+
+Node* createNode(int);
+Node* insertNode(Node*, int);
+void bfsTraversal(Node*);
+
+int main()
+{
+    Node* root = nullptr;
+    
+    root = insertNode(root, 42);
+    root = insertNode(root, 13);
+    root = insertNode(root, 11);
+    root = insertNode(root, 10);
+    root = insertNode(root, 28);
+    root = insertNode(root, 51);
+    root = insertNode(root, 171);
+    
+    cout << "BFS Traversal: ";
+    bfsTraversal(root);
+    cout << endl;
+    
+    return 0;
+}
+
+Node* createNode(int iValue)
+{
+    Node* tmp = (Node*) malloc(sizeof(Node));
+    
+    if (tmp == nullptr)
+    {
+        cerr << "Erro em createNode: malloc" << endl;
+        exit(1);
+    }
+    
+    tmp->iPayload = iValue;
+    tmp->ptrLeft = nullptr;
+    tmp->ptrRight = nullptr;
+    
+    return tmp;
+}
+
+Node* insertNode(Node* startingNode, int iData)
+{
+    if(startingNode == nullptr)
+    {
+        return createNode(iData);
+    }
+    
+    if(iData < startingNode->iPayload)
+    {
+        startingNode->ptrLeft = insertNode(startingNode->ptrLeft, iData);
+    }
+    else
+    {
+        startingNode->ptrRight = insertNode(startingNode->ptrRight, iData);
+    }
+    
+    return startingNode;
+}
+
+void bfsTraversal(Node* startingNode)
+{
+    cout << "Olá" << endl;
+}
+
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+
+#include <iostream>
+
+using namespace std;
+
+typedef struct Node
+{
+    int iPayload;
+    Node* ptrLeft;
+    Node* ptrRight;
+} Node;
+
+Node* createNode(int);
+Node* insertNode(Node*, int);
+void bfsTraversal(Node*);
+int treeHeight(Node*);
+
+int main()
+{
+    Node* root = nullptr;
+    
+    root = insertNode(root, 42);
+    root = insertNode(root, 13);
+    root = insertNode(root, 11);
+    root = insertNode(root, 10);
+    root = insertNode(root, 28);
+    root = insertNode(root, 51);
+    root = insertNode(root, 171);
+    
+    cout << "BFS Traversal: ";
+    bfsTraversal(root);
+    cout << endl;
+    
+    cout << "Tree Height: " << treeHeight(root) << endl;
+    
+    return 0;
+}
+
+Node* createNode(int iValue)
+{
+    Node* tmp = (Node*) malloc(sizeof(Node));
+    
+    if (tmp == nullptr)
+    {
+        cerr << "Erro em createNode: malloc" << endl;
+        exit(1);
+    }
+    
+    tmp->iPayload = iValue;
+    tmp->ptrLeft = nullptr;
+    tmp->ptrRight = nullptr;
+    
+    return tmp;
+}
+
+Node* insertNode(Node* startingNode, int iData)
+{
+    if(startingNode == nullptr)
+    {
+        return createNode(iData);
+    }
+    
+    if(iData < startingNode->iPayload)
+    {
+        startingNode->ptrLeft = insertNode(startingNode->ptrLeft, iData);
+    }
+    else
+    {
+        startingNode->ptrRight = insertNode(startingNode->ptrRight, iData);
+    }
+    
+    return startingNode;
+}
+
+void bfsTraversal(Node* startingNode)
+{
+    if (startingNode == nullptr) return;
+    
+    // Parte 1 do Trabalho: Alterar para Lista Encadeada
+    Node* nodeQueue[100];
+    int iQueueFront = 0;
+    int iQueueRear = 0;
+    
+    nodeQueue[iQueueRear] = startingNode;
+    iQueueRear++;
+    
+    while (iQueueFront < iQueueRear)
+    {
+        Node* currentNode = nodeQueue[iQueueFront];
+        iQueueFront++;
+        
+        cout << currentNode->iPayload << " ";
+        
+        if (currentNode->ptrLeft != nullptr)
+        {
+            nodeQueue[iQueueRear] = currentNode->ptrLeft;
+            iQueueRear++;
+        }
+        
+        if (currentNode->ptrRight != nullptr)
+        {
+            nodeQueue[iQueueRear] = currentNode->ptrRight;
+            iQueueRear++;
+        }
+        
+    }
+}
+
+// Parte 2 do Trabalho: Elaborar busca utilizando BFS (vocês já fizeram o DFS)
+// Parte 3 do Trabalho: Monitorar o desempenho de busca em árvore utilizando DFS e BFS
+// Parte 4 do Trabalho: Monitorar o desempenho de criação de listas
+// Parte 5 do Trabalho: Monitorar o desempenho de criação de árvores
+
+int treeHeight(Node* startingNode)
+{
+    if (startingNode == nullptr) return 0;
+    else
+    {
+        int iLeftHeight = treeHeight(startingNode->ptrLeft);
+        int iRightHeight = treeHeight(startingNode->ptrRight);
+        
+        return max(iLeftHeight, iRightHeight) + 1;
+    }
+}
+
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
