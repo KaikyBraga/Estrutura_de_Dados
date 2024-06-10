61bc722d291843dc369e8b8cbac6fe3f099ff85c
Kaiky Braga <126474113+KaikyBraga@users.noreply.github.com>
2024-06-10 09:04:37 -0300

    Create aula-21.cpp

---
+++ aula-21.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void moveDisk(char, char, unsigned int);
+void solveHanoi(unsigned int, char, char, char);
+
+int main()
+{
+    
+    unsigned omt uiNumber = 1;
+    
+    cout << "Digite o número de discos: ";
+    cin >> uiNumber;
+    solveHanoi(uiNumber, 'A', 'B', 'C')
+    
+    
+    
+    return 0;
+}
+
+
+void moveDisk(char fromRod, char toRod, unsigned int Disk)
+{
+        
+}
+
+
+
+void solveHanoi(unsigned int numberOfDisks, char fromRod, char topRod, char auxRod);
+{
+    
+}
